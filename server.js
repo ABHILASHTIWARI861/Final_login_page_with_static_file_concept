7f3982727c231ca80001dc4f16ef68311b339c22
abhilash tiwari <abhilashtiwari.smn@gmail.com>
2025-07-09 14:10:33 +0530

    done

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public')); // express ko btana hoga hai ki public folder se static files(style.css),(login.jpg) serve karni hai,wrna cssfile ya image file load hi nhi lega.
 
+
+//"jb tum get method se index.html file bulaoge tb web page sab kuch fetch kr lega except  html me rakhi static files like css,image files etc
+// static file ko load nhi lega ,aise case ke tumko express ko btana hoga hai ki public folder se static files(style.css),(login.jpg) serve karni hai,wrna css file ya image file load hi nhi lega website/webpage pr.
 app.post('/submit', (req, res) => {
    const e_mail_id=req.body.e_mail;
     const password=req.body.paass;
