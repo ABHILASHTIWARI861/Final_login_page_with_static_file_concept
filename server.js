a01604a911f13b3ac5ec72faccd9b229da7ff024
abhilash tiwari <abhilashtiwari.smn@gmail.com>
2025-07-09 14:07:01 +0530

    done

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public')); // express ko btana hoga hai ki public folder se static files(style.css),(login.jpg) serve karni hai,wrna cssfile ya image file load hi nhi lega.
+
+app.post('/submit', (req, res) => {
+   const e_mail_id=req.body.e_mail;
+    const password=req.body.paass;
+
+    if (e_mail_id === 'abhilashtiwari.smn@gmail.com' && password === 'Abhi32@#'){
+        res.send('welcome to the login page');
+    } 
+    else {
+       res.send('Invalid email or password');
+   }
+});
+
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, () => {
+console.log('Server is running on port 3000');
+});
